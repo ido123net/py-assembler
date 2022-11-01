a61978a2069c5859b4d6a787bb0137b1d7866806
Ido Frenkel <Ido123net@gmail.com>
2022-11-01 10:48:05 +0200

    add instruction line parse func

--- py_assembler/parser.py
+++ py_assembler/parser.py
@@ -1,4 +1,10 @@
+from __future__ import annotations
+
+import re
 from enum import Enum
+
+from py_assembler.instruction import Instruction
+from py_assembler.instruction import INSTRUCTIONS
 
 
 class LineType(Enum):
@@ -15,3 +21,23 @@ def line_type(line: str) -> LineType:
         return LineType.DIRECTIVE
     else:
         return LineType.INSTRUCTION
+
+
+def is_valid_label(label: str) -> bool:
+    if len(label) > 31 or len(label.split()) != 1:
+        return False
+
+    for instruction_type in INSTRUCTIONS:
+        if label in instruction_type:
+            return False
+
+    return re.compile(r"[a-zA-Z][a-zA-Z0-9]*").match(label) is not None
+
+
+def instruction_line_parse(instruction_line: str) -> Instruction:
+    if ":" in instruction_line:
+        label, _, instruction = instruction_line.partition(":")
+    else:
+        label, instruction = None, instruction_line
+    cmd, *args = re.split(r"[\s,]+", instruction.strip())
+    return Instruction(label, cmd, args)
--- tests/parser_test.py
+++ tests/parser_test.py
@@ -1,36 +1,97 @@
+from __future__ import annotations
+
 import pytest
-from py_assembler.parser import LineType, line_type
+
+from py_assembler.instruction import Instruction
+from py_assembler.parser import instruction_line_parse
+from py_assembler.parser import is_valid_label
+from py_assembler.parser import line_type
+from py_assembler.parser import LineType
 
 
 @pytest.mark.parametrize(
-    ('input_s', 'expected'),
+    ("input_s", "expected"),
     (
-        (';this is a commant', LineType.COMMENT),
-        ('  ; ', LineType.COMMENT),
-        ('          ', LineType.COMMENT),
-        ('', LineType.COMMENT),
-        ('.entry Next', LineType.DIRECTIVE),
-        ('.extern wNumber', LineType.DIRECTIVE),
+        (";this is a commant", LineType.COMMENT),
+        ("  ; ", LineType.COMMENT),
+        ("          ", LineType.COMMENT),
+        ("", LineType.COMMENT),
+        (".entry Next", LineType.DIRECTIVE),
+        (".extern wNumber", LineType.DIRECTIVE),
         ('STR:    .asciz  "aBcd"', LineType.DIRECTIVE),
-        ('MAIN:   add     $3,$5,$9', LineType.INSTRUCTION),
-        ('LOOP:   ori     $9,-5,$2', LineType.INSTRUCTION),
-        ('        la      val1', LineType.INSTRUCTION),
-        ('        jmp     Next', LineType.INSTRUCTION),
-        ('Next:   move    $20,$4', LineType.INSTRUCTION),
-        ('LIST:   .db     6,-9', LineType.DIRECTIVE),
-        ('        bgt     $4,$2,END', LineType.INSTRUCTION),
-        ('        la      K', LineType.INSTRUCTION),
-        ('        sw      $0,4,$10', LineType.INSTRUCTION),
-        ('        bne     $31,$9,LOOP', LineType.INSTRUCTION),
-        ('        call    val1', LineType.INSTRUCTION),
-        ('        jmp     $4', LineType.INSTRUCTION),
-        ('        la      wNumber', LineType.INSTRUCTION),
-        ('.extern val1', LineType.DIRECTIVE),
-        ('        .dh     27056', LineType.DIRECTIVE),
-        ('K:      .dw     31,-12', LineType.DIRECTIVE),
-        ('END:    stop', LineType.INSTRUCTION),
-        ('.entry K', LineType.DIRECTIVE)
-    )
+        ("MAIN:   add     $3,$5,$9", LineType.INSTRUCTION),
+        ("LOOP:   ori     $9,-5,$2", LineType.INSTRUCTION),
+        ("        la      val1", LineType.INSTRUCTION),
+        ("        jmp     Next", LineType.INSTRUCTION),
+        ("Next:   move    $20,$4", LineType.INSTRUCTION),
+        ("LIST:   .db     6,-9", LineType.DIRECTIVE),
+        ("        bgt     $4,$2,END", LineType.INSTRUCTION),
+        ("        la      K", LineType.INSTRUCTION),
+        ("        sw      $0,4,$10", LineType.INSTRUCTION),
+        ("        bne     $31,$9,LOOP", LineType.INSTRUCTION),
+        ("        call    val1", LineType.INSTRUCTION),
+        ("        jmp     $4", LineType.INSTRUCTION),
+        ("        la      wNumber", LineType.INSTRUCTION),
+        (".extern val1", LineType.DIRECTIVE),
+        ("        .dh     27056", LineType.DIRECTIVE),
+        ("K:      .dw     31,-12", LineType.DIRECTIVE),
+        ("END:    stop", LineType.INSTRUCTION),
+        (".entry K", LineType.DIRECTIVE),
+    ),
 )
 def test_line_type(input_s, expected):
     assert line_type(input_s) == expected
+
+
+@pytest.mark.parametrize(
+    ("input_s", "expected"),
+    (
+        ("validLabel", True),
+        ("hEllo", True),
+        ("x", True),
+        ("He78902", True),
+        ("", False),
+        ("contain space", False),
+        ("1startWithNumber", False),
+        ("veryveryveryveryveryverylonglabel", False),
+    ),
+)
+def test_is_valid_label(input_s, expected):
+    assert is_valid_label(input_s) == expected
+
+
+@pytest.mark.parametrize(
+    ("input_s", "expected"),
+    (
+        (
+            "MAIN:   add     $3,$5,$9",
+            Instruction("MAIN", "add", ["$3", "$5", "$9"]),
+        ),
+        (
+            "LOOP:   ori     $9,-5,$2",
+            Instruction("LOOP", "ori", ["$9", "-5", "$2"]),
+        ),
+        (
+            "la val1",
+            Instruction(None, "la", ["val1"]),
+        ),
+        (
+            "   jmp	   Next",
+            Instruction(None, "jmp", ["Next"]),
+        ),
+        (
+            "Next:   move    $20,$4",
+            Instruction("Next", "move", ["$20", "$4"]),
+        ),
+        (
+            "	bgt	$4,	$2,	END",
+            Instruction(None, "bgt", ["$4", "$2", "END"]),
+        ),
+        (
+            "END:    stop",
+            Instruction("END", "stop", []),
+        ),
+    ),
+)
+def test_instruction_line_parse(input_s, expected):
+    assert instruction_line_parse(input_s) == expected
